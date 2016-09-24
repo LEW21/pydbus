e06bde5b5262cabdc947e14b75ea327fb29b1cb0
Linus Lewandowski <lew21@xtreeme.org>
2016-09-24 21:48:41 +0200

    Simplify green.sleep.

--- pydbus/green.py
+++ pydbus/green.py
@@ -1,4 +1,19 @@
 try:
-	from .green_greenlet import GreenFunc, sleep, spawn_in_green_thread
+	from .green_greenlet import GreenFunc, spawn_in_green_thread
 except ImportError:
-	from .green_sync import GreenFunc, sleep, spawn_in_green_thread
+	from .green_sync import GreenFunc, spawn_in_green_thread
+
+from gi.repository import GLib
+
+def sleep_start(seconds, cb):
+	def sleep_cb():
+		cb(None, None)
+		return False
+
+	GLib.timeout_add(seconds * 1000, sleep_cb)
+
+sleep_finish = (lambda x: x)
+
+from time import sleep as sleep_sync
+
+sleep = GreenFunc(sleep_start, sleep_finish, sleep_sync)
