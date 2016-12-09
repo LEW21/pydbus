f4787f9694773e966232e999e71a1e5312546550
Alberto Caso <alberto.caso@adaptia.es>
2016-12-09 23:36:05 +0100

    Refactor tests runner script to use dbus-launch instead of dbus-daemon

--- tests/run.sh
+++ tests/run.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 set -e
 
-ADDRESS_FILE=$(mktemp /tmp/pydbustest.XXXXXXXXX)
-PID_FILE=$(mktemp /tmp/pydbustest.XXXXXXXXX)
+eval `dbus-launch --sh-syntax`
 
-dbus-daemon --session --print-address=0 --print-pid=1 --fork 0>"$ADDRESS_FILE" 1>"$PID_FILE"
-
-export DBUS_SESSION_BUS_ADDRESS=$(cat "$ADDRESS_FILE")
-PID=$(cat "$PID_FILE")
-
-trap 'kill -TERM $PID' EXIT
-
-rm "$ADDRESS_FILE" "$PID_FILE"
+trap 'kill -TERM $DBUS_SESSION_BUS_PID' EXIT
 
 PYTHON=${1:-python}
 
