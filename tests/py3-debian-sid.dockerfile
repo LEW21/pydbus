152de2232adfffb1889f38091bcf617b169add78
Harry G. Coin <hgcoin@gmail.com>
2017-06-17 00:15:22 -0500

    change sid servers, add relative module to _unittest

--- tests/py3-debian-sid.dockerfile
+++ tests/py3-debian-sid.dockerfile
@@ -1,6 +1,7 @@
 FROM debian:sid
+RUN apt-get install -y netselect-apt
+RUN netselect-apt -n -s sid
 RUN apt-get update
-
 RUN apt-get install -y dbus  psmisc dbus-x11 python3  python3-pip python3-dev python3-gi    libglib2.0 libglib2.0-dev gobject-introspection  python-gi-dev
 RUN python3 --version
 RUN pip3 install --upgrade pip
--- tests/unittest_server.py
+++ tests/unittest_server.py
@@ -5,9 +5,9 @@ Created on Jun 16, 2017
 '''
 from pydbus import SessionBus
 from gi.repository.GLib import MainLoop
-from pydbus.extensions.PatchPreGlib246 import compat_dbus_connection_register_object # @UnresolvedImport @Reimport @UnusedImport
-from pydbus.extensions.PatchPreGlib246 import compat_dbus_invocation_return_value  # @UnresolvedImport @Reimport @UnusedImport
-from pydbus.extensions.PatchPreGlib246 import compat_dbus_invocation_return_dbus_error  # @UnresolvedImport @Reimport @UnusedImport
+from .extensions.PatchPreGlib246 import compat_dbus_connection_register_object # @UnresolvedImport @Reimport @UnusedImport
+from .extensions.PatchPreGlib246 import compat_dbus_invocation_return_value  # @UnresolvedImport @Reimport @UnusedImport
+from .extensions.PatchPreGlib246 import compat_dbus_invocation_return_dbus_error  # @UnresolvedImport @Reimport @UnusedImport
 
 
 class PyDbusUnitTestService(object):
