b0db181d00e2602560fe982ef76dbc530694f2f8
Harry G. Coin <hgcoin@gmail.com>
2017-06-17 01:01:22 -0500

    unit test script again.

--- tests/run.sh
+++ tests/run.sh
@@ -11,13 +11,8 @@ PYTHON=${1:-python}
 
 "$PYTHON" $TESTS_DIR/context.py
 "$PYTHON" $TESTS_DIR/identifier.py
-#if [ "$2" != "dontpublish" ]
-#then
-	"$PYTHON" $TESTS_DIR/publish.py
-	"$PYTHON" $TESTS_DIR/publish_properties.py
-	"$PYTHON" $TESTS_DIR/publish_multiface.py
-#fi
-echo running unit tests
-cd $SRC_DIR/pydbus
+"$PYTHON" $TESTS_DIR/publish.py
+"$PYTHON" $TESTS_DIR/publish_properties.py
+"$PYTHON" $TESTS_DIR/publish_multiface.py
 "$PYTHON"  $SRC_DIR/pydbus/_unittest.py
 
