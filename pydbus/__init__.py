3dd4dc672b3c695ed26ec39b3d4537de1641245a
Matthias Dellweg <2500@gmx.de>
2017-10-31 17:04:37 +0100

    Introduce DBusException for propagating DBusErrors
    DBusException can be subclassed, to indicate exceptions, that are meant to
    be translated to DBusErrors.
    The type of error is encoded by the class variable 'dbus_name'.
    The class variable 'silent' can be used to suppress logging.

--- pydbus/__init__.py
+++ pydbus/__init__.py
@@ -1,4 +1,5 @@
 from .bus import SystemBus, SessionBus, connect
+from .exceptions import DBusException
 from gi.repository.GLib import Variant
 
-__all__ = ["SystemBus", "SessionBus", "connect", "Variant"]
+__all__ = ["SystemBus", "SessionBus", "connect", "DBusException", "Variant"]
