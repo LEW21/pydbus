ed9aed64aced2e1ff6e97f7e09889a278b7531cb
Florent Thiéry <florent.thiery@ubicast.eu>
2019-05-23 15:03:03 +0200

    pep8 cleanups, allow installation from any folder

--- setup.py
+++ setup.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import os
+
+os.chdir(os.path.dirname(__file__))
 
 with open('README.rst') as f:
-	readme = f.read()
+    readme = f.read()
 
 setup(
-	name = "pydbus",
-	version = "0.6.0",
-	description = "Pythonic DBus library",
-	long_description = readme,
-	author = "Linus Lewandowski",
-	author_email = "linus@lew21.net",
-	url = "https://github.com/LEW21/pydbus",
-	keywords = "dbus",
-	license = "LGPLv2+",
+    name='pydbus',
+    version='0.6.0',
+    description='Pythonic DBus library',
+    long_description=readme,
+    author='Linus Lewandowski',
+    author_email='linus@lew21.net',
+    url='https://github.com/LEW21/pydbus',
+    keywords='dbus',
+    license='LGPLv2+',
 
-	packages = ["pydbus"],
-	package_data = {"": ["LICENSE"]},
-	package_dir = {"pydbus": "pydbus"},
-	zip_safe = True,
-	classifiers = [
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7'
-	]
+    packages=['pydbus'],
+    package_data={'': ['LICENSE']},
+    package_dir={'pydbus': 'pydbus'},
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
